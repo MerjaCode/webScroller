57abe80de2486d76e312005312c6f446e7877450
Carsten Brooks <cbroosk56@gmail.copm>
2021-08-16 10:25:11 -0600

    Fixed Refresh Bug

---
+++ run.bat
@@ -0,0 +1,3 @@
+cd d:
+cd chuck\Programming\webScroller
+python3 web_scroller.py
--- web_scroller.py
+++ web_scroller.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import json
 from time import sleep
+import concurrent.futures
 
 print("Starting Web Scroller")
 
@@ -9,7 +10,8 @@ file = open("./urlList.json")
 jsnFile = json.load(file)
 url_List = [url for url in jsnFile["url_list"]]
 
-driver.maximize_window() # maximize the browser window
+driver.maximize_window()  # maximize the browser window
+
 
 # Open all urls in the json file
 
@@ -20,38 +22,39 @@ for i in range(len(url_List)):
         print("Opening a new window...")
         driver.execute_script("window.open('');")
         driver.switch_to.window(driver.window_handles[-1])
-    
+
+
 # Custom code for each window
 
-  # weather radar
+# weather radar
 driver.switch_to.window(driver.window_handles[1])
-fullscreen_btn  = "/html/body/div/div[5]/div[1]/div[1]/div[1]/div[2]/div[2]"
-zoom_btn        = "/html/body/div/div[5]/div[1]/div[1]/div[1]/div[2]/div[4]/div[3]/div[1]/div/button[1]"
-play_btn        = "/html/body/div/div[5]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]"
+fullscreen_btn = "/html/body/div/div[5]/div[1]/div[1]/div[1]/div[2]/div[2]"
+zoom_btn = "/html/body/div/div[5]/div[1]/div[1]/div[1]/div[2]/div[4]/div[3]/div[1]/div/button[1]"
+play_btn = "/html/body/div/div[5]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]"
 driver.find_element_by_xpath(fullscreen_btn).click()
-for i in range(6): driver.find_element_by_xpath(zoom_btn).click()
+for i in range(6):
+    driver.find_element_by_xpath(zoom_btn).click()
 driver.find_element_by_xpath(play_btn).click()
 
-  # Wheat prices 
+# Wheat prices
 driver.switch_to.window(driver.window_handles[2])
-cookie_btn      = "//*[@id=\"pardotCookieButton\"]"
+cookie_btn = "//*[@id=\"pardotCookieButton\"]"
 driver.find_element_by_xpath(cookie_btn).click()
 
-  # Wheat graph
+# Wheat graph
 driver.switch_to.window(driver.window_handles[3])
 driver.find_element_by_xpath(cookie_btn).click()
 
-  # Farm weather station
+# Farm weather station
 driver.switch_to.window(driver.window_handles[4])
-user_input      = "//*[@id=\"username\"]"
-pass_input      = "//*[@id=\"password\"]"
-login_btn       = "//*[@id=\"submit\"]"
-uname           = "chuckmerja"
-passwd          = "soilmoisture"
+user_input = "//*[@id=\"username\"]"
+pass_input = "//*[@id=\"password\"]"
+login_btn = "//*[@id=\"submit\"]"
+uname = "chuckmerja"
+passwd = "soilmoisture"
 driver.find_element_by_xpath(user_input).send_keys(uname)
 driver.find_element_by_xpath(pass_input).send_keys(passwd)
 driver.find_element_by_xpath(login_btn).click()
-
 
 
 # Cycle through tabs
@@ -61,16 +64,15 @@ count = 1
 refresh = False
 while True:
     try:
-        if count == 10:
-            refresh = True 
+        if count == 5:
+            refresh = True
             count = -1
         for i in range(len(url_List)):
-            if refresh:
-                driver.refresh()
             driver.switch_to.window(driver.window_handles[i])
             sleep(wait_time)
+            if refresh and i == 0 or refresh and i == 2:
+                driver.refresh()
         count += 1
     except Exception as e:
         print(e)
         break
-
